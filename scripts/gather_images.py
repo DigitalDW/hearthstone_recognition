8d02c01fed9cdbf400f2faaa33957faa5a5a1a84
DigitalDW <loris.rimaz@gmail.com>
2020-05-19 11:47:20 +0200

    unified image download code

--- scripts/gather_images.py
+++ scripts/gather_images.py
@@ -9,13 +9,22 @@ t = len(cards)
 for card in cards:
     t -= 1
     _id = card["id"]
-    url = "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{}.png".format(
+    url256 = "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{}.png".format(
         _id)
+    url512 = "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{}.png".format(
+        _id)
+
     try:
-        r = requests.get(url, stream=True)
-        r.raw.decode_content = True
+        r256 = requests.get(url256, stream=True)
+        r256.raw.decode_content = True
         with open("../images/raw/256x/{}.png".format(_id), 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
+            shutil.copyfileobj(r256.raw, f)
+
+        r512 = requests.get(url256, stream=True)
+        r512.raw.decode_content = True
+        with open("../images/raw/512x/{}.png".format(_id), 'wb') as f:
+            shutil.copyfileobj(r512.raw, f)
+
         print("success! {}.png saved! {} to go".format(_id, t))
     except:
         print("failed")
